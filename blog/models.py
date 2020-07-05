598fb1d283a6642552bff4f8ade1856e444f49b5
LeechenLove <727084494@qq.com>
2020-07-05 15:09:35 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from django.utils.html import strip_tags
+
+import markdown
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    # 文章标题
+    title = models.CharField('标题', max_length=70)
+
+    # 文章正文
+    body = models.TextField('正文')
+
+    # 创建时间和最后一次修改时间
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
+
+    # 文章摘要，允许为空
+    excerpt = models.CharField('摘要',max_length=200, blank=True)
+
+    # 分类和标签
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+
+    # 文章作者
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+    def save(self, *args, **kwargs):
+        '''复写保存的方法'''
+        self.modified_time = timezone.now()
+
+        # 实例一个Markdown类，用于渲染body的文本
+        md = markdown.Markdown(extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+
+                                      ])
+        # strip_tags 去掉 HTML 文本的全部 HTML 标签
+        self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        '''自定义获取相对url路径'''
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,59 @@
+import re
+import markdown
+
+from django.shortcuts import render, get_object_or_404
+from django.utils.text import slugify
+
+from markdown.extensions.toc import TocExtension
+
+from .models import Post, Category, Tag
+
+
+# 博客首页函数
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# 文章详情页面函数
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+                             'markdown.extensions.extra',
+                             'markdown.extensions.codehilite',
+                             TocExtension(slugify=slugify),
+                         ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+    return render(request, 'blog/detail.html', context={'post': post})
+
+
+# 归档视图
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    )
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# 分类视图
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# 标签视图
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# 作者视图
+def author(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post_list = Post.objects.filter(author=post.author)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
