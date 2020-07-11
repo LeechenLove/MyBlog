e0dad66ab41f033a4e55bd981468f8997092a180
LeechenLove <727084494@qq.com>
2020-07-11 11:42:43 +0800

    modify secretkey

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,5 +1,5 @@
 from .common import *
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'h7)41f=uzf7wqtb(wrc80qo8y@vsz@pg6t9rggxe^tni3u1f$r'
 DEBUG = False
 ALLOWED_HOSTS = ['47.104.1.188']
